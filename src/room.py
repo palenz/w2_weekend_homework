6308fafae320a51d2f9bbef0cf07d5d062cba00d
Juan <jpalenzuela@outlook.com>
2020-11-29 17:50:28 +0000

    MVP completed

--- src/room.py
+++ src/room.py
@@ -2,9 +2,9 @@ from src.guest import Guest
 
 class Room:
 
-    def __init__(self, room_name, room_song):
+    def __init__(self, room_name):
         self.room_name = room_name
-        self.room_song = room_song
+        self.room_songs = []
         self.room_guests = []
 
     def check_in_guest(self, guest):
@@ -12,3 +12,6 @@ class Room:
 
     def check_out_guest(self, guest):
         self.room_guests.remove(guest)
+
+    def add_song_to_room(self, song):
+        self.room_songs.append(song)
--- tests/room_test.py
+++ tests/room_test.py
@@ -7,21 +7,26 @@ class TestRoom(unittest.TestCase):
 # 1. Create rooms
 
     def test_create_room(self):
-        new_room = Room('The Heavy Room', 'Duality')
+        new_room = Room('The Heavy Room')
         self.assertEqual('The Heavy Room', new_room.room_name)
 
     def test_check_in(self):
         new_guest = Guest('Jimmy')
-        room_1 = Room('The Heavy Room', 'Duality')
+        room_1 = Room('The Heavy Room')
         room_1.check_in_guest(new_guest)
         self.assertEqual(True, (new_guest in room_1.room_guests))
 
     def test_check_out(self):
         new_guest = Guest('Jimmy')
-        room_1 = Room('The Heavy Room', 'Duality')
+        room_1 = Room('The Heavy Room')
         room_1.check_in_guest(new_guest)
         room_1.check_out_guest(new_guest)
         self.assertEqual(False, (new_guest in room_1.room_guests))
+
+    def test_add_songs_to_rooms(self):
+        room_1 = Room('The Heavy Room')
+        room_1.add_song_to_room('Duality')
+        self.assertEqual(True, 'Duality' in room_1.room_songs)
 
 
     
